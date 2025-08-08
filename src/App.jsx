3940e3822712e7f1e31abefb4a3f076696eb283d
FelipeIE <felipecorpiefe@Gmail.com>
2025-08-08 10:02:08 -0300

    First Commit - Arquivo todo configurado com fonts e variabeis de cores instaladas

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from './pages/Home/Home.jsx';
+import './App.scss';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
