4092d771665bb6a7cbeaf1ad62eb4d1c8ebccea5
FelipeIE <felipecorpiefe@Gmail.com>
2025-08-08 12:14:55 -0300

    experience component created

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Experience.scss';
+
+import CSS from '../../assets/IconsExperience/css.png';
+import HTML from '../../assets/IconsExperience/html.png';
+import JS from '../../assets/IconsExperience/javascript.png';
+import REACT from '../../assets/IconsExperience/reactjs.png';
+import WORDPRESS from '../../assets/IconsExperience/wordpress.png';
+
+
+export default function Experience() {
+    return (
+        <div className="experiencecontainerPrincipal">
+            <p>EXPERIENCIA COM</p>
+            <div className="experiencecontainerImg">
+                <img src={WORDPRESS} alt="Icone Wordpress" />
+                <img src={HTML} alt="Icone HTML" />
+                <img src={CSS} alt="Icone CSS" />
+                <img src={JS} alt="Icone Javascript" />
+                <img src={REACT} alt="Icone React" />
+            </div>
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header.jsx'
 import Apresentation from '../../components/Apresentation/Apresentation.jsx'
+import Experience from '../../components/Experience/Experience.jsx'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Apresentation />
+      <Experience />
     </>
   )
 }
