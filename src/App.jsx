067137df07c0889f8ffce69c9ccae7829b6ea25c
FelipeIE <felipecorpiefe@Gmail.com>
2025-08-09 12:40:25 -0300

    configurando o Ga4

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useRef, useState } from 'react';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from './pages/Home/Home.jsx';
 import './App.scss';
+import ReactGA from "react-ga4";
+
 
 function App() {
-
+  ReactGA.initialize("G-21CHVKG7FN");
+  ReactGA.send({ hitType: "pageview", page: "Pagina Incial", title: "Custom Title" });
   return (
     <>
       <BrowserRouter>
