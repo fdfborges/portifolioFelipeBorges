44eb53227fb99b478876f2f2cd282905ac295204
Felipe Borges <felipedefreitasborges@gmail.com>
2025-08-23 13:03:10 +0000

    Configurando props Experiencie

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,7 +8,9 @@ export default function Experience() {
         <>
             <div className="divcontainerExperience">
                 <p>EXPERIENCIA </p>
-                <ExperienceCard />
+                <ExperienceCard TituloCard={"Lead Software Engineer at Google"} ConteudoCard={"As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide."} Data={"Nov 2019 - Present"} LogoEmpresa={"https://www.gstatic.com/marketing-cms/assets/images/d5/dc/cfe9ce8b4425b410b49b7f2dd3f3/g.webp=s48-fcrop64=1,00000000ffffffff-rw"}/>
+                <ExperienceCard TituloCard={"Lead Software Engineer at Google"} ConteudoCard={"As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide."} Data={"Nov 2019 - Present"} LogoEmpresa={"https://www.gstatic.com/marketing-cms/assets/images/d5/dc/cfe9ce8b4425b410b49b7f2dd3f3/g.webp=s48-fcrop64=1,00000000ffffffff-rw"}/>
+                <ExperienceCard TituloCard={"Lead Software Engineer at Google"} ConteudoCard={"As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide."} Data={"Nov 2019 - Present"} LogoEmpresa={"https://www.gstatic.com/marketing-cms/assets/images/d5/dc/cfe9ce8b4425b410b49b7f2dd3f3/g.webp=s48-fcrop64=1,00000000ffffffff-rw"}/>
             </div>
         </>
     )
